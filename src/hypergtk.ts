4f03df83f0716b11064d6aa0e9db7f092616eb04
Thiago Jedi <thiagoa7@gmail.com>
2019-11-04 21:51:34 -0300

    Only render on render

--- src/hypergtk.ts
+++ src/hypergtk.ts
@@ -1,30 +1,40 @@
 var Gtk = imports.gi.Gtk;
 
-function createWidget(widgetConstructor, attributes, ...children) {
-    const widget = new widgetConstructor({ visible: true, ...attributes });
-
-    if (children) {
-        if (widget.add === undefined)
-            throw new Error('Cannot add child to non Container widget');
-
-        children
-            .reduce((acc, val) => acc.concat(val), [])
-            .map((child) => typeof child === 'string' ? new Gtk.Label({ label: child, visible: true }) : child)
-            .forEach((child) => widget.add(child));
-    }
-
-    return widget;
+function createWidget(widgetConstructor, attributes, ...args) {
+  const children = args.length ? [].concat(args) : null;
+  return { widgetConstructor, attributes, children };
 }
 
-function render(widget, app) {
-    if (app !== undefined) {
-        if (!(widget instanceof Gtk.Window))
-            throw new Error('Root element must be a Gtk.Window')
+function isConstructor(f) {
+  try {
+    new f();
+  } catch (err) {
+    return false;
+  }
+  return true;
+}
 
-        app.add_window(widget);
-    } else {
-        if (!(widget instanceof Gtk.ApplicationWindow))
-            throw new Error('Root element must be a Gtk.Window or Gtk.ApplicationWindow with the application prop')
-    }
-    widget.present();
+function render({ widgetConstructor, attributes, children }) {
+  if (!isConstructor(widgetConstructor))
+    return render(widgetConstructor(attributes));
+
+  const widget = new widgetConstructor({ visible: true, ...attributes });
+
+  if (children) {
+    if (widget.add === undefined)
+      throw new Error("Cannot add child to non Container widget");
+
+    children
+      .reduce((acc, val) => acc.concat(val), [])
+      .map(child =>
+        typeof child === "string"
+          ? new Gtk.Label({ label: child, visible: true })
+          : render(child)
+      )
+      .forEach(child => widget.add(child));
+  }
+
+  if (typeof widget.present === "function") widget.present();
+
+  return widget;
 }
